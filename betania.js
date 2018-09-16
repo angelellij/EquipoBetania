f14f1de39077056518834ee08c01e8ba8f9dfb97
angelellij <42815097+angelellij@users.noreply.github.com>
2018-09-15 21:53:50 -0300

    Add files via upload

---
+++ betania.js
@@ -0,0 +1,43 @@
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+var si = 1.8*w/1295
+var sm = 8*w/1400
+var smme = 980*w/1295
+var smma = 1928*w/1295
+
+console.log(w)
+console.log(h)
+
+$(document).ready(function(){
+	$("header").on("click","img",function(){
+		$('#aanav').toggleClass('closed')
+	});
+})
+
+if (w > 800){
+
+	$(window).scroll(function(){
+		var wScroll = $(this).scrollTop();
+
+		$('#titulos').css({
+		'transform': 'translate(0px,'+ wScroll/si +'%)'
+		});
+
+		console.log(wScroll)
+
+		if(wScroll> $('#quienesp').offset().top && wScroll< $('#servicios').offset().top) {
+		$('#mision').css({
+		'transform': 'translate(0px,'+(wScroll-1560)/sm +'%)'
+		});
+		
+		$('#vision').css({
+		'transform': 'translate(0px,'+(wScroll-1560)/sm +'%)'
+		});
+		
+		$('#valores').css({
+		'transform': 'translate(0px,'+(wScroll-1560)/sm +'%)'
+		});
+		}
+	});
+}
